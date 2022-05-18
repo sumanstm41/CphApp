e431eed33df9a42d7a3075674dd2bb3a522d3668
sumanstm41 <suman.magar@patientsky.com>
2022-05-18 10:20:09 +0200

    Components - Hooks Basic children

--- src/components/RnComponentsHooks.tsx
+++ src/components/RnComponentsHooks.tsx
@@ -1,11 +1,36 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {FC} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+// https://psdev.nu/confluence/display/VAS/____Polymorphic+components+with+Typescript
+// Polymorphic components with Typescript
+interface IProps {
+  children: any;
+}
+const Page1 = () => <Text>Page 1</Text>;
+const Page2 = () => <Text>Page 2</Text>;
+const Page3 = () => <Text>Page 3</Text>;
+
+const Wizard: FC<IProps> = ({children}) => {
+  const contents = React.Children.toArray(children);
+  const currentContent = contents[0];
+  return (
+    <SafeAreaView>
+      <Text>{currentContent}</Text>
+    </SafeAreaView>
+  );
+};
 
 const RnComponentsHooks = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>RnComponentsHooks</Text>
-    </View>
+      <Wizard>
+        <Page1 />
+        <Page2 />
+        <Page3 />
+      </Wizard>
+    </SafeAreaView>
   );
 };
 
